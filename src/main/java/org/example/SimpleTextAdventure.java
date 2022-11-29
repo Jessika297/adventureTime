359049b8488144f90532a358a625dffe04f52a6e
Jess <jessika.29.7.99@hotmail.de>
2022-11-29 08:47:27 +0100

    initial commit

--- src/main/java/org/example/SimpleTextAdventure.java
+++ src/main/java/org/example/SimpleTextAdventure.java
@@ -1,50 +1,39 @@
 package org.example;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
-/**
- * Fragt den Spieler in welche Richtung er weitergehen will.
- */
 public class SimpleTextAdventure {
-    /**
-     * Starte das mini TextAdventure
-     * @param args
-     */
+
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // Gebe dem Spieler eine Ausgabe vor
-        System.out.println( "Du stehst in einem Wald. Links geht es nicht weiter. Du kannst aber geradeaus oder rechts lang gehen." );
-        System.out.println( "Wofür entscheidest du dich?" );
 
-        // Versuche seine Eingabe zu lesen, zum Beispiel mit Hilfe des BufferedReaders (kann Zeilenweise lesen)
-        try ( BufferedReader input = new BufferedReader( new InputStreamReader(System.in) ) ){
-            // die eingelesene Zeile ist erst mal null
-            String zeile = null;
+        System.out.println("Du stehst in einem Wald. Links geht es nicht weiter. Du kannst aber geradeaus oder rechts lang gehen.");
+        System.out.println("Wofür entscheidest du dich?");
 
-            // solange diese Zeile null ist tue folgendes
-            while ( zeile == null ){
-                // lese die Eingabe des Spielers
-                zeile = input.readLine();
+        try (Scanner input = new Scanner(new InputStreamReader(System.in))) {
 
-                if ( zeile.matches( "geradeaus" ) ){
-                    // GEH GERADE AUS
+            String line = null;
+
+            while (line == null) {
+                line = input.nextLine();
+
+                if (line.contains("geradeaus")) {
                     System.out.println("Du gehst geradeaus!");
-                } else if ( zeile.matches( "rechts" ) ){
-                    // GEH RECHTS LANG
-                    System.out.println( "Du gehst rechts rum." );
-                } else if ( zeile.matches( "dreh um" ) ){
-                    // DREH UM
+                } else if (line.contains("rechts")) {
+                    System.out.println("Du gehst rechts rum.");
+                } else if (line.contains("dreh um")) {
                     System.out.println("Du drehst dich um.");
-                } else { // immer daran denken, dass der Spieler auch falsche Eingaben tätigen kann. Das sollte man auch abhandeln.
-                    zeile = null;
-                    System.out.println( "Das geht leider nicht. Möchtest du gerade aus gehen, schreibe 'geradeaus'." + System.lineSeparator()
-                            + "Möchtest du rechts entlang gehen, schreibe 'rechts rum'." + System.lineSeparator()
+                } else {
+                    line = null;
+                    System.out.println("Das geht leider nicht. Möchtest du gerade aus gehen, schreibe 'geradeaus'." + System.lineSeparator()
+                            + "Möchtest du rechts entlang gehen, schreibe 'rechts'." + System.lineSeparator()
                             + "Möchtest du umdrehen, schreibe 'umdrehen'." + System.lineSeparator()
                             + "Wofür entscheidest du dich?");
-                } // schon fertig...
+                }
             }
-        } catch (IOException e) { // Fehler abhandeln nicht vergessen.
-            e.printStackTrace();
         }
-    }}
+    }
+}
